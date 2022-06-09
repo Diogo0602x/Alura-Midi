501ef03ade6d5d4c2ed39f7cbd97ccf6035e79b0
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-06-09 11:38:28 -0300

    Aula 3 terminada

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
