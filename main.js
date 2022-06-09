94e6e59a1282f50f0773940c38af7a6862340f37
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-06-09 12:37:32 -0300

    Aula 4 terminada

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElemntoAudio) {
+    document.querySelector(idElemntoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0
+
+// enquanto 
+for (let contador = 0; contador <  listaDeTeclas.length; contador ++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    
+}
