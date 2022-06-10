333ab0581143385aba3d7ce29294e646787c3108
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-06-10 13:15:07 -0300

    Aula 5 e curso terminado

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-function tocaSom (idElemntoAudio) {
-    document.querySelector(idElemntoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }    
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
-
-// enquanto 
-for (let contador = 0; contador <  listaDeTeclas.length; contador ++) {
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    
+
+    tecla.onkeydown = function (evento) {
+        
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+
 }
